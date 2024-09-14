cf790e0b4b3c620e462ae03d5b19c7217db5637a
CheBeomHee <tigerfrom2@naver.com>
2024-09-14 20:15:54 +0900

    fix: 충돌 오류해결

--- src/main/java/gilBeot/Course/Repository/ToiletRepository.java
+++ src/main/java/gilBeot/Course/Repository/ToiletRepository.java
@@ -2,6 +2,8 @@ package gilBeot.Course.Repository;
 
 import gilBeot.Course.model.toilet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ToiletRepository extends JpaRepository<toilet, Long> {
 }
