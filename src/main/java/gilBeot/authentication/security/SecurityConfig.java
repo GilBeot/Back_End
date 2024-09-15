5ce5f0c2109c9d8ac59bc792b853d5a613c0760e
KimJinSe0ng <waujoxc@gmail.com>
2024-09-15 18:35:27 +0900

    refactor: 권한 설정 수정

--- src/main/java/gilBeot/authentication/security/SecurityConfig.java
+++ src/main/java/gilBeot/authentication/security/SecurityConfig.java
@@ -76,8 +76,7 @@ public class SecurityConfig {
         //경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/login", "/", "/join").permitAll()
-                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .requestMatchers("/login", "/", "/join", "/admin").permitAll()
                         .anyRequest().authenticated()
                 );
 
--- src/main/java/gilBeot/board/controller/BoardController.java
+++ src/main/java/gilBeot/board/controller/BoardController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/board")
+@RequestMapping("/api/board")
 @RequiredArgsConstructor
 public class BoardController {
 
